b0f686c6723e61b9e32d9e6dc116e9828069aeb5
Nitsan Avni <nitsanav@gmail.com>
2022-04-29 00:13:42 +0300

    cli responds to input keypress

--- src/cli.mts
+++ src/cli.mts
@@ -1,3 +1,25 @@
+// import os
+import os from "os";
 import logUpdate from "log-update";
+import _ from "lodash";
 
-logUpdate("Hello World");
+process.openStdin();
+if (process.stdin.isTTY) {
+  process.stdin.setRawMode(true);
+}
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (text: string) => {
+  if (text === "\u0003" || text === "q") {
+    process.exit();
+  }
+
+  logUpdate(
+    _.join(
+      _.times(2, () => _.join(_.times(2, () => text))),
+      os.EOL
+    )
+  );
+});
+
+logUpdate("∙");
--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,10 +1,27 @@
-import { $ } from "zx";
+import { $, sleep } from "zx";
 import test from "ava";
+import { promisify } from "util";
 
 test("cli", async (t) => {
-  const { stdout } = await $`node ../src/cli.mjs`;
+  const { stdout, stdin, exitCode } = $`node ../src/cli.mjs`;
 
-  t.regex(stdout, /Hello World/);
+  const getOut = promisify((cb) =>
+    stdout.once("data", (data) => cb(null, String(data).trim()))
+  ) as () => Promise<string>;
+
+  const d = await getOut();
+
+  t.deepEqual(d, "∙");
+
+  stdin.write("a");
+  t.regex(await getOut(), /a,a\na,a$/s);
+
+  stdin.write("b");
+  t.regex(await getOut(), /b,b\nb,b$/s);
+
+  stdin.write("q");
+
+  t.is(await exitCode, 0);
 });
 
 test("sanity", async (t) => {
